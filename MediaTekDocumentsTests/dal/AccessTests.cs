b67319d137d0c11d77241c4484c716722a6c6bcd
fanny <fanny@PC-FANNY>
2025-03-06 14:18:42 +0100

    Mission 2 - Gérer les commandes : commandes de livres, de dvd, et des revues

---
+++ MediaTekDocumentsTests/dal/AccessTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MediaTekDocuments.dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaTekDocuments.dal.Tests
+{
+    [TestClass()]
+    public class AccessTests
+    {
+        private static readonly Access access = Access.GetInstance();
+
+        [TestMethod()]
+        public void ParutionDansAbonnementTest()
+        {
+            DateTime dateCommande = new DateTime(2025, 03, 05);
+            DateTime dateParution = new DateTime(2025, 03, 06);
+            DateTime dateFinAbonnement = new DateTime(2025, 03, 07);
+
+            bool expected = true;
+            bool actual = access.ParutionDansAbonnement(dateCommande, dateFinAbonnement, dateParution);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
