3d1663886f4580378423329b715c7ae6026b53e1
fanny <fanny@PC-FANNY>
2025-03-18 10:44:06 +0100

    Mission 6 - Tâche 1 : gérer les tests

--- MediaTekDocumentsTests/dal/AccessTests.cs
+++ MediaTekDocumentsTests/dal/AccessTests.cs
@@ -7,18 +7,24 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MediaTekDocuments.dal.Tests
-{
+{ 
+    /// <summary>
+    /// Tests sur la classe d'accès aux données Access
+    /// </summary>
     [TestClass()]
     public class AccessTests
     {
         private static readonly Access access = Access.GetInstance();
 
+        /// <summary>
+        /// Test sur la méthode ParutionDansAbonnement
+        /// </summary>
         [TestMethod()]
         public void ParutionDansAbonnementTest()
         {
-            DateTime dateCommande = new DateTime(2025, 03, 05);
-            DateTime dateParution = new DateTime(2025, 03, 06);
-            DateTime dateFinAbonnement = new DateTime(2025, 03, 07);
+            DateTime dateCommande = new DateTime(2025, 03, 05, 0, 0, 0, DateTimeKind.Local);
+            DateTime dateParution = new DateTime(2025, 03, 06, 0, 0, 0, DateTimeKind.Local);
+            DateTime dateFinAbonnement = new DateTime(2025, 03, 07, 0, 0, 0, DateTimeKind.Local);
 
             bool expected = true;
             bool actual = access.ParutionDansAbonnement(dateCommande, dateFinAbonnement, dateParution);
